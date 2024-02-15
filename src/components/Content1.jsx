76256c0777c65e6b18912a423730dbe5a99ff46c
Nikola Zovko <nikolajoe95@gmail.com>
2024-02-15 08:06:31 +0100

    new

---
+++ src/components/Content1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Content1 = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic vitae error distinctio quis adipisci facilis dolore accusantium optio quia quisquam!</p>
+        </div>
+    )
+}
+
+export default Content1
